f698943c1e75d3df6257e2e331b54ec283ce32f5
Jake Manley <jbmanley45@gmail.com>
2022-12-12 12:21:12 -0800

    completed

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,6 @@
 class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :length, :description, :poster_url, :category, :discount
-  
+  # shows associate nested director data when navigating to /movies
+  belongs_to :director
+  has_many :reviews
 end
